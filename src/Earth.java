c06f482413e6637a296e64086699a12ce13b25db
ElenaS1981 <elenasidyakina@gmail.com>
2021-10-17 17:48:35 -0700

    Section 1 Lecture 2 code

---
+++ src/Earth.java
@@ -0,0 +1,14 @@
+public class Earth {
+    public static void main(String[] args) {
+        Human tom = new Human("Tom");
+        tom.age = 4;
+        tom.eyeColor = "green";
+        tom.heightInInches = 70;
+
+
+        Human je = new Human("Je");
+        je.age =6;
+    }
+
+
+}
---
+++ src/Human.java
@@ -0,0 +1,16 @@
+public class Human {
+    int age;
+    String eyeColor;
+    int heightInInches;
+    String name;
+
+    public Human(String name){
+        this.name = name;
+    }
+
+    public void speak(){
+        System.out.println("My name is " + name);
+
+    }
+
+}
