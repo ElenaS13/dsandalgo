d9c8de2c1bf6d81a91c95f38f05f0dd9542501a6
ElenaS1981 <elenasidyakina@gmail.com>
2021-10-21 14:13:28 -0700

    practice for item removal from array

---
+++ src/Zoo/removeArray.java
@@ -0,0 +1,16 @@
+package Zoo;
+
+import java.util.Arrays;
+
+public class removeArray {
+    public static void main(String[] args) {
+        int[] arr = {23,45,67,232,4,5,21,35,56,53};
+        int removeIndex = 2;
+
+        for(int i = removeIndex; i<arr.length-1; i++){
+            arr[i] = arr[i+1];
+        }
+
+        System.out.println("After removal " + Arrays.toString(arr) );
+    }
+}
